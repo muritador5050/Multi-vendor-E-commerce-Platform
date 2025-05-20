91f50ecabf3870a4a20fe30613bf8014bf64b15e
muritador5050 <muritador5050@gmail.com>
2025-05-20 17:38:24 +0100

    solved router error on payment route page

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,6 @@ router.use('/', require('./products.routes'));
 router.use('/', require('./category.routes'));
 router.use('/reviews', require('./review.routes'));
 router.use('/orders', require('./order.routes'));
+router.use('/payments', require('./payment.routes'));
 
 module.exports = router;
