56d6f0173a013ff73f1c9f79f669bff519854b6e
muritador5050 <muritador5050@gmail.com>
2025-06-01 12:14:43 +0100

    i added app.js back to my file

--- server/server.js
+++ server/server.js
@@ -1,39 +1,5 @@
-require('./controllers/passport');
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const { globalErrorHandler } = require('./utils/globalErrorHandler');
-const passport = require('passport');
-const { PORT, FRONTEND_URL } = require('./configs');
-const connectDB = require('./database/index');
-
-connectDB();
-
-const app = express();
-
-// Middleware setup
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(cookieParser());
-app.use(morgan('dev'));
-app.use(passport.initialize());
-
-//middlewares
-app.use(
-  cors({
-    origin: FRONTEND_URL || 'http://localhost:3000',
-    credentials: true,
-  })
-);
-//Routes
-app.use('/uploads', express.static('uploads'));
-app.use('/', require('./routes'));
-
-// Error handling middleware
-app.use(globalErrorHandler);
+const { PORT } = require('./configs');
+const app = require('../app');
 
 //Server
 app.listen(PORT, () => {
