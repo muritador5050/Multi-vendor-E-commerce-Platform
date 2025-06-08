e8088fd653b77cca0a6f52957bbb4503d6a8bd7f
muritador5050 <muritador5050@gmail.com>
2025-06-08 16:40:43 +0100

    first commit in client side

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import User from './pages/user';
+
+function App() {
+  return (
+    <div className='App'>
+      <User />
+      <footer className='footer'>
+        <p>
+          Powered by <a href='https://reactjs.org'>React</a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/user.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@chakra-ui/react';
+
+function User() {
+  return (
+    <div>
+      <Text fontSize='2xl' fontWeight='bold' mb={4}>
+        User Page
+      </Text>
+    </div>
+  );
+}
+
+export default User;
