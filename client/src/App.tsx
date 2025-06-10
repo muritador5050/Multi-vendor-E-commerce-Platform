e50cc4f348ac85cde2ff9a82cddb3730c59c9786
muritador5050 <muritador5050@gmail.com>
2025-06-10 19:59:21 +0100

    i created login and signUp page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,28 +7,28 @@ import ContactUs from './pages/contactUs';
 import StoreManager from './pages/storeManager';
 import VendorMembership from './pages/vendorMemberShip';
 import Layout from './components/layout/layout';
-import AccountPage from '../authentication/accountPage';
+import AccountPage from './authentication/accountPage';
 import WishList from './pages/wishList';
 
 //App
 function App() {
   return (
     <Router>
-      {/* <Stack minH='100vh'> */}
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path='blog' element={<Blog />} />
-          <Route path='shop' element={<Shop />} />
-          <Route path='vendor-membership' element={<VendorMembership />} />
-          <Route path='store-manager' element={<StoreManager />} />
-          <Route path='contact-us' element={<ContactUs />} />
-          <Route path='wishlist' element={<WishList />} />
-          <Route path='*' element={<Box>404 Not Found</Box>} />
-        </Route>
-        <Route path='/account' element={<AccountPage />} />
-      </Routes>
-      {/* </Stack> */}
+      <Stack minH='100vh'>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path='blog' element={<Blog />} />
+            <Route path='shop' element={<Shop />} />
+            <Route path='vendor-membership' element={<VendorMembership />} />
+            <Route path='store-manager' element={<StoreManager />} />
+            <Route path='contact-us' element={<ContactUs />} />
+            <Route path='wishlist' element={<WishList />} />
+            <Route path='*' element={<Box>404 Not Found</Box>} />
+          </Route>
+          <Route path='/account' element={<AccountPage />} />
+        </Routes>
+      </Stack>
     </Router>
   );
 }
