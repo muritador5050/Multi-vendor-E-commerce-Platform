c4ae4c7989fb363f6b365d2bdece3a567609d5ce
muritador5050 <muritador5050@gmail.com>
2025-09-16 13:25:14 +0100

    i added new domain to cors setting

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ const corsOptions = {
 
     const allowedOrigins = [
       'http://localhost:5173',
-      'https://multi-vendor-e-commerce-platform.vercel.app',
+      'https://www.multivendor.site',
     ];
 
     if (allowedOrigins.includes(origin)) {
